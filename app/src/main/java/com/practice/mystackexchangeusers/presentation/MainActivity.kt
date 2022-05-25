2f360d5d2c7dab99ddab36b7602e414e405620db
sarjshah <sarju@novoda.com>
2022-05-25 12:08:31 +0100

    Change package structure and apply Clean Architecture.

--- app/src/main/java/com/practice/mystackexchangeusers/presentation/MainActivity.kt
+++ app/src/main/java/com/practice/mystackexchangeusers/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.practice.mystackexchangeusers
+package com.practice.mystackexchangeusers.presentation
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.practice.mystackexchangeusers.R
 import com.practice.mystackexchangeusers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
