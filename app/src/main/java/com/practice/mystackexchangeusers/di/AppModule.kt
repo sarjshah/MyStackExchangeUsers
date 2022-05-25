4e4298cf8e96fdca92a2d46b8dbec6c7416b248f
sarjshah <sarju@novoda.com>
2022-05-25 15:44:36 +0100

    Add Hilt DI

---
+++ app/src/main/java/com/practice/mystackexchangeusers/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.practice.mystackexchangeusers.di
+
+import com.practice.mystackexchangeusers.common.Constants
+import com.practice.mystackexchangeusers.data.remote.StackExchangeApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApuService() = Retrofit.Builder()
+        .baseUrl(Constants.STACK_EXCHANGE_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(StackExchangeApi::class.java)
+}
