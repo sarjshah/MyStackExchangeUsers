4d01681aa8c049ef9cd5095cc073ed4d531593c8
sarjshah <sarju@novoda.com>
2022-05-25 15:43:47 +0100

    Create Service layer.

--- app/src/main/java/com/practice/mystackexchangeusers/common/Constants.kt
+++ app/src/main/java/com/practice/mystackexchangeusers/common/Constants.kt
@@ -1,3 +1,6 @@
 package com.practice.mystackexchangeusers.common
 
-object Constants
+object Constants {
+    const val STACK_EXCHANGE_BASE_URL = "https://api.stackexchange.com/2.2/"
+    const val STACK_EXCHANGE_SITE = "stackoverflow"
+}
---
+++ app/src/main/java/com/practice/mystackexchangeusers/data/remote/StackExchangeApi.kt
@@ -0,0 +1,22 @@
+package com.practice.mystackexchangeusers.data.remote
+
+import com.practice.mystackexchangeusers.common.Constants
+import com.practice.mystackexchangeusers.data.remote.dto.UserResponse
+import com.practice.mystackexchangeusers.data.remote.dto.UsersResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface StackExchangeApi {
+
+    @GET("users")
+    suspend fun getUsers(): UsersResponse
+
+    @GET("users/{userId}")
+    suspend fun getUser(
+        @Path("userId")
+        id: String,
+        @Query("site")
+        site: String = Constants.STACK_EXCHANGE_SITE
+    ): UserResponse
+}
