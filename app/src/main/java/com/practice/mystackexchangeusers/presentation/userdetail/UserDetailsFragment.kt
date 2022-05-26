3a966a6dffe629cf1f724e3a5f8c4cb3e5c1c134
sarjshah <sarju@novoda.com>
2022-05-26 09:11:04 +0100

    Rename Fragments.

--- app/src/main/java/com/practice/mystackexchangeusers/presentation/userdetail/UserDetailsFragment.kt
+++ app/src/main/java/com/practice/mystackexchangeusers/presentation/userdetail/UserDetailsFragment.kt
@@ -1,20 +1,20 @@
 package com.practice.mystackexchangeusers.presentation.userdetail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.practice.mystackexchangeusers.R
-import com.practice.mystackexchangeusers.databinding.FragmentSecondBinding
+import com.practice.mystackexchangeusers.databinding.FragmentUserDetailsBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class UserDetailsFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentUserDetailsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentUserDetailsBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -34,7 +34,7 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.action_UserDetailsFragment_to_UsersFragment)
         }
     }
 
--- app/src/main/java/com/practice/mystackexchangeusers/presentation/userlist/UsersFragment.kt
+++ app/src/main/java/com/practice/mystackexchangeusers/presentation/userlist/UsersFragment.kt
@@ -1,20 +1,24 @@
 package com.practice.mystackexchangeusers.presentation.userlist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.practice.mystackexchangeusers.R
-import com.practice.mystackexchangeusers.databinding.FragmentFirstBinding
+import com.practice.mystackexchangeusers.databinding.FragmentUsersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+@AndroidEntryPoint
+class UsersFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private val viewModel: UsersViewModel by viewModels()
+    private var _binding: FragmentUsersBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +29,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentUsersBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -34,8 +38,9 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_UsersFragment_to_UserDetailsFragment)
         }
+
     }
 
     override fun onDestroyView() {
