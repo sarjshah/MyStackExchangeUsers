8cdc0c3904b15101b8d2f0c117d53e34602e8b04
sarjshah <sarju@novoda.com>
2022-05-26 17:26:47 +0100

    Implement onClick and navigation to userdetails fragment.

--- app/src/main/java/com/practice/mystackexchangeusers/presentation/userlist/UsersFragment.kt
+++ app/src/main/java/com/practice/mystackexchangeusers/presentation/userlist/UsersFragment.kt
@@ -12,11 +12,13 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.practice.mystackexchangeusers.R
 import com.practice.mystackexchangeusers.databinding.FragmentUsersBinding
 import com.practice.mystackexchangeusers.presentation.adapters.UsersAdapter
+import com.practice.mystackexchangeusers.presentation.userlist.UsersViewModel.Action
+import com.practice.mystackexchangeusers.presentation.userlist.UsersViewModel.ViewEvent
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 /**
@@ -46,15 +48,27 @@ class UsersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        usersAdapter = UsersAdapter()
         setupRecyclerView()
         setupData()
+        setUpEventFlow()
+    }
 
-
-        binding.btnSearch.setOnClickListener {
-            findNavController().navigate(R.id.action_UsersFragment_to_UserDetailsFragment)
+    private fun setUpEventFlow() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.viewEvent.collectLatest { event ->
+                    when (event) {
+                        is ViewEvent.GoToUserDetails -> {
+                            val action =
+                                UsersFragmentDirections.actionUsersFragmentToUserDetailsFragment(
+                                    event.user
+                                )
+                            findNavController().navigate(action)
+                        }
+                    }
+                }
+            }
         }
-
     }
 
     private fun setupData() {
@@ -64,10 +78,10 @@ class UsersFragment : Fragment() {
                     when (state) {
                         is UsersViewModel.ViewState.Content -> {
                             binding.pbLoading.visibility = View.GONE
-                            usersAdapter.userList = state.users.users
+                            usersAdapter.userList = state.users
                         }
                         is UsersViewModel.ViewState.Error -> {
-                            Toast.makeText(context, "${state.error}", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(context, "Error occurred: ${state.error}", Toast.LENGTH_SHORT).show()
                         }
                         UsersViewModel.ViewState.Loading -> {
                             binding.pbLoading.visibility = View.VISIBLE
@@ -80,8 +94,14 @@ class UsersFragment : Fragment() {
 
     private fun setupRecyclerView() {
         binding.rvUsers.apply {
+            usersAdapter = UsersAdapter()
             adapter = usersAdapter
             layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+
+        usersAdapter.onUserClickedListener = { user ->
+            viewModel.send(Action.UserClicked(user))
         }
     }
 
--- app/src/main/java/com/practice/mystackexchangeusers/presentation/userlist/UsersViewModel.kt
+++ app/src/main/java/com/practice/mystackexchangeusers/presentation/userlist/UsersViewModel.kt
@@ -3,13 +3,15 @@ package com.practice.mystackexchangeusers.presentation.userlist
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.practice.mystackexchangeusers.common.Resource
-import com.practice.mystackexchangeusers.domain.model.Users
+import com.practice.mystackexchangeusers.domain.model.User
 import com.practice.mystackexchangeusers.domain.usecase.UserUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,8 +21,17 @@ class UsersViewModel @Inject constructor(
     private val _viewState: MutableStateFlow<ViewState> = MutableStateFlow(ViewState.Loading)
     val viewState: Flow<ViewState> = _viewState
 
+    private val _viewEvent = MutableSharedFlow<ViewEvent>()
+    val viewEvent: Flow<ViewEvent> = _viewEvent
+
     init {
         loadContent()
+    }
+
+    fun send(action: Action) = viewModelScope.launch {
+        when (action) {
+            is Action.UserClicked -> _viewEvent.emit(ViewEvent.GoToUserDetails(action.user))
+        }
     }
 
     private fun loadContent() =
@@ -30,14 +41,23 @@ class UsersViewModel @Inject constructor(
                 is Resource.Error -> ViewState.Error(
                     result.message ?: "An unknown error has occurred"
                 )
-                is Resource.Success -> ViewState.Content(result.data ?: Users(emptyList()))
+                is Resource.Success -> {
+                    ViewState.Content(result.data?.users ?: emptyList())
+                }
             }
         }.launchIn(viewModelScope)
 
+    sealed class ViewEvent() {
+        data class GoToUserDetails(val user: User) : ViewEvent()
+    }
 
     sealed class ViewState() {
         object Loading : ViewState()
         data class Error(val error: String) : ViewState()
-        data class Content(val users: Users) : ViewState()
+        data class Content(val users: List<User>) : ViewState()
+    }
+
+    sealed class Action() {
+        data class UserClicked(val user: User) : Action()
     }
 }
