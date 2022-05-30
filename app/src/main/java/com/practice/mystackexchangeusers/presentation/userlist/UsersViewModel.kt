6810b858f5c82aa375a1546f0f11401642465aeb
sarjshah <sarju@novoda.com>
2022-05-30 12:50:42 +0100

    Add clear for search functionality.

--- app/src/main/java/com/practice/mystackexchangeusers/presentation/userlist/UsersViewModel.kt
+++ app/src/main/java/com/practice/mystackexchangeusers/presentation/userlist/UsersViewModel.kt
@@ -2,6 +2,7 @@ package com.practice.mystackexchangeusers.presentation.userlist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.practice.mystackexchangeusers.common.DispatcherProvider
 import com.practice.mystackexchangeusers.common.Resource
 import com.practice.mystackexchangeusers.domain.model.User
 import com.practice.mystackexchangeusers.domain.usecase.UserUseCases
@@ -10,6 +11,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -17,12 +19,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class UsersViewModel @Inject constructor(
-    private val useCases: UserUseCases
+    private val useCases: UserUseCases,
+    dispatchers: DispatcherProvider
 ) : ViewModel() {
     private val _viewState: MutableStateFlow<ViewState> = MutableStateFlow(ViewState.Loading)
     val viewState: Flow<ViewState> = _viewState
 
-    private var rawList = MutableStateFlow(emptyList<User>())
+    private val _rawList = MutableStateFlow(emptyList<User>())
 
     private val _viewEvent = MutableSharedFlow<ViewEvent>()
     val viewEvent: Flow<ViewEvent> = _viewEvent
@@ -39,15 +42,16 @@ class UsersViewModel @Inject constructor(
             _viewState.value = ViewState.Filtered(true)
         } else {
             queryStateFlow.value = ""
+            _viewState.value = ViewState.Content(_rawList.value.prepareForView(USER_FETCH_SIZE))
             _viewState.value = ViewState.Filtered(false)
-            _viewState.value = ViewState.Content(rawList.value.prepareForView(USER_FETCH_SIZE))
         }
     }
 
-    private val _filteredResult = queryStateFlow.combine(rawList) { query, users ->
+    private val _filteredResult = queryStateFlow.combine(_rawList) { query, users ->
         users.filter { user -> user.userName.startsWith(query, ignoreCase = true) }
             .prepareForView(USER_FETCH_SIZE)
-    }
+    }.flowOn(dispatchers.main)
+
     val filteredResult = _filteredResult
 
     fun send(action: Action) = viewModelScope.launch {
@@ -65,7 +69,7 @@ class UsersViewModel @Inject constructor(
                     result.message ?: "An unknown error has occurred"
                 )
                 is Resource.Success -> {
-                    rawList.value = result.data ?: emptyList()
+                    _rawList.value = result.data ?: emptyList()
                     ViewState.Content(
                         result.data?.prepareForView(USER_FETCH_SIZE) ?: emptyList()
                     )
@@ -73,7 +77,8 @@ class UsersViewModel @Inject constructor(
             }
         }.launchIn(viewModelScope)
 
-    private fun List<User>.prepareForView(userFetchSize: Int) = sortedBy { it.userName }.take(userFetchSize)
+    private fun List<User>.prepareForView(userFetchSize: Int) =
+        sortedBy { it.userName }.take(userFetchSize)
 
     sealed class ViewEvent() {
         data class GoToUserDetails(val user: User) : ViewEvent()
